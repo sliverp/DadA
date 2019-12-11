b0ffdcf457917ea3c8c382b095d15f08a5f1c52f
why724809024 <hbhs6855@163.com>
2019-12-11 16:53:06 +0800

    Rename Option

---
+++ Edit/Edit/Lex.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Edit.Data;
+
+namespace Edit
+{
+    class Sign {
+        public String id;
+        public String mean;
+        public MateData content;
+        public int instenceNum;
+        public Sign(MateData content)
+        {
+            this.content = content;
+        }
+        public Sign(String mean)
+        {
+            this.mean = mean;
+        }
+    }
+    class SignTable: List<Sign>
+    {
+        public bool isInSignTable(String id)
+        {
+            return false;
+        }
+       
+
+        public MateData getData(String id)
+        {
+            Sign s=this.Find((c) => c.id == id);
+            return s.content;
+        }
+    }
+    class Lex
+    {
+        public List<Operation> optionsList = new List<Operation>();
+        public List<List<String>> sentencec = new List<List<string>>();
+        public Lex(String program)
+        {
+            List<String> sentences = program.Split(';').ToList();
+            foreach(String sentence in sentences)
+            {
+                List<String> words = sentence.Split(' ').ToList();
+                this.sentencec.Add(words);
+            }
+        }
+        public static bool isKeyWord(String s)
+        {
+
+            return true;
+        }
+        public void test()//以下全是test!!!!!!!!!!
+        {
+            //f(){
+            //    return 1 + 2;
+            //}
+            //a = f();
+            SignTable signs = new SignTable();
+
+            DadaInt a = new DadaInt("a");
+            a.setData("456789");
+            signs.Add(new Sign(a));
+
+            signs.Add(new Sign("+"));
+
+            DadaInt b = new DadaInt("b");
+            b.setData("456789");
+            signs.Add(new Sign(b));
+           
+           
+            Function f = new Function("f");
+            List<Operation> options = new List<Operation>();
+            AssignOpthon assignOpthon = new AssignOpthon(signs);
+            options.Add(assignOpthon);
+            f.setOptionList(options);
+
+
+            SignTable signs2 = new SignTable();
+
+            Sign s = new Sign(new DadaInt("s"));
+            s.mean = "结果";
+            signs2.Add(s);
+
+
+            signs2.Add(new Sign(f));
+
+            AssignOpthon assignOpthon2 = new AssignOpthon(signs2);
+
+            assignOpthon2.doSomethings();
+
+
+
+            //assignOpthon.doSomethings();
+            //MateData mateData = assignOpthon.result;
+
+        }
+    }
+}
---
+++ Edit/Edit/Operation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Edit.Data;
+
+namespace Edit
+{
+    abstract class Operation
+    {
+        public String mean;
+        abstract public void doSomethings();
+        public MateData result;
+    }
+
+    //举个例子,这个操作其实没有写完
+    class AssignOpthon : Operation
+    {
+        private MateData assigned;//需要被赋值的对象;
+        private SignTable signTable;//符号表
+        private List<MateData> dataList;
+        public AssignOpthon(SignTable signTable)
+        {
+            this.signTable = signTable;
+            this.mean = "赋值";
+        }
+        public override void doSomethings()
+        {
+            foreach(Sign sign in signTable)
+            {
+                if (sign.content is Function)
+                {
+                    Function f = (Function)sign.content;
+                    MateData fResult = f.run();
+                    sign.content = fResult;
+                   
+                }
+            }
+            String s = "";
+            foreach(Sign sign in signTable)
+            {
+                if (sign.mean == "结果") continue;
+                if(sign.content is Hashable)
+                {
+                        s = s + sign.content.toString();
+                }
+                else if (sign.content == null)
+                {
+                    s = s + sign.mean;
+                }
+
+            }
+            DataTable dataTable = new DataTable();
+            double x = double.Parse(dataTable.Compute(s, null).ToString());
+            DadaInt data = new DadaInt("ddd");
+            data.setData(x.ToString());
+            Sign ss = signTable.Find((e) => e.mean == "结果");
+            if (ss != null) {
+                ss.content = data;
+            }
+            else
+            {
+                result = data;
+                this.mean = "返回";
+            }
+               
+        }
+    }
+
+    class ReturnOpthon : Operation
+    {
+        MateData returnDate;
+        public override void doSomethings()
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
